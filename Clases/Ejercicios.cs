f11933a6d610163663c7de61b4683feaacf78634
LDVP5049 <luismaster752@gmail.com>
2023-09-29 10:52:01 -0700

    Agregar archivos de proyecto.

---
+++ Clases/Ejercicios.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Clases
+{
+    internal class Ejercicios
+    {
+        //LOOGICA
+        /*
+        public int Intervalo(int numeroInicio, int numeroFinal)
+        {
+            if (numeroInicio >= 0)
+            {
+                for (int i=0; i < numeroFinal; i++)
+                {
+                    return Intervalo(numeroInicio - 1, i);
+                }
+            }
+        }*/
+        public void TablaMulti(int num, int numeroMulti)
+        {
+            if (numeroMulti <= 10)
+            {
+                int resultado = num * numeroMulti;
+                Console.WriteLine($"{num} * {numeroMulti} = {resultado}");
+                TablaMulti(num, numeroMulti + 1);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Test.Clases;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //INSTANCIAS
+
+           /* Ejercicios intervalo = new Ejercicios();
+            Console.WriteLine("Numero inicio");
+            int numeroInicio = int.Parse(Console.ReadLine());
+            Console.WriteLine("Numero final");
+            int numeroFinal = int.Parse(Console.ReadLine());
+            intervalo.Intervalo(numeroInicio, numeroFinal);*/
+
+            Ejercicios multi =  new Ejercicios();
+            Console.WriteLine("Ingresa un numero");
+            int num = int.Parse(Console.ReadLine());
+            multi.TablaMulti(num);
+            
+
+
+
+           
+        }
+    }
+}
